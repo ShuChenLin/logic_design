a1ee84d48d90176db5384dd40d5958ecb05d4012
ShuChenLin <ch25585.leader.tw@gmail.com>
2022-09-24 19:22:31 +0800

    dmux_testbench

---
+++ Lab1_Team25_Dmux_1x4_4bit_t.v
@@ -0,0 +1,31 @@
+`timescale 1ns/1ps
+
+module Dmux_1x4_4bit_t;
+reg [3:0] in = 4'b1;
+reg [1:0] sel = 2'b0;
+
+wire [3:0] a, b, c, d;
+
+Dmux_1x4_4bit m1(
+    .in (in),
+    .a (a),
+    .b (b),
+    .c (c),
+    .d (d),
+    .sel (sel)
+);
+
+initial begin
+    $dumpfile("DMUX.vcd");
+    $dumpvars("+all");
+end
+
+initial begin
+    repeat (2 ** 3) begin
+        #1 sel = sel + 2'b1;
+        in = in + 4'b1;
+    end
+    #1 $finish;
+end
+
+endmodule

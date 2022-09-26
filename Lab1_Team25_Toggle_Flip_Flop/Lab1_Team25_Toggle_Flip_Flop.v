16ed09876aa18ce4ab307480ddb83c48a98e5528
ShuChenLin <ch25585.leader.tw@gmail.com>
2022-09-26 11:28:26 +0800

    add tab

--- Lab1_Team25_Toggle_Flip_Flop/Lab1_Team25_Toggle_Flip_Flop.v
+++ Lab1_Team25_Toggle_Flip_Flop/Lab1_Team25_Toggle_Flip_Flop.v
@@ -1,48 +1,48 @@
 `timescale 1ns/1ps
 
 module D_Latch(e, d, q);
-input e;
-input d;
-output q;
+    input e;
+    input d;
+    output q;
 
-wire a, b, c, dn;
+    wire a, b, c, dn;
 
-not tmp0(dn, d);
-and tmp1(a, d, e);
-and tmp2(b, dn, e);
-nor tmp3(c, q, a);
-nor tmp4(q, c, b);
+    not tmp0(dn, d);
+    and tmp1(a, d, e);
+    and tmp2(b, dn, e);
+    nor tmp3(c, q, a);
+    nor tmp4(q, c, b);
 
 endmodule
 
 module D_Flip_Flop(clk, d, q);
-input clk;
-input d;
-output q;
+    input clk;
+    input d;
+    output q;
 
-wire t, clkn;
+    wire t, clkn;
 
-not tmp5(clkn, clk);
-D_Latch tmp6(clkn, d, t);
-D_Latch tmp7(clk, t, q);
+    not tmp5(clkn, clk);
+    D_Latch tmp6(clkn, d, t);
+    D_Latch tmp7(clk, t, q);
 
 endmodule
 
 module Toggle_Flip_Flop(clk, q, t, rst_n);
-input clk;
-input t;
-input rst_n;
-output q;
+    input clk;
+    input t;
+    input rst_n;
+    output q;
 
-wire q_n, t_n, qtn, tqn, tqxor, d;
+    wire q_n, t_n, qtn, tqn, tqxor, d;
 
-not m1(q_n, q);
-not m2(t_n, t);
-and m3(qtn, q, t_n);
-and m4(tqn, t, q_n);
-or m5(tqxor, qtn, tqn);
-and m6(d, rst_n, tqxor);
+    not m1(q_n, q);
+    not m2(t_n, t);
+    and m3(qtn, q, t_n);
+    and m4(tqn, t, q_n);
+    or m5(tqxor, qtn, tqn);
+    and m6(d, rst_n, tqxor);
 
-D_Flip_Flop m7(clk, d, q);
+    D_Flip_Flop m7(clk, d, q);
 
 endmodule
--- Lab1_Team25_Toggle_Flip_Flop/Lab1_Team25_Toggle_Flip_Flop_t.v
+++ Lab1_Team25_Toggle_Flip_Flop/Lab1_Team25_Toggle_Flip_Flop_t.v
@@ -1,30 +1,30 @@
 `timescale 1ns/1ps
 
 module Toggle_Flip_Flop_t;
-reg clk = 1'b0;
-reg t = 1'b0;
-reg rst_n = 1'b0;
-wire q;
+    reg clk = 1'b0;
+    reg t = 1'b0;
+    reg rst_n = 1'b0;
+    wire q;
 
-always#(1) clk = ~clk;
+    always#(1) clk = ~clk;
 
-Toggle_Flip_Flop m1(
-    .clk (clk),
-    .q (q),
-    .t (t),
-    .rst_n (rst_n)
-);
+    Toggle_Flip_Flop m1(
+        .clk (clk),
+        .q (q),
+        .t (t),
+        .rst_n (rst_n)
+    );
 
-initial begin
-    $dumpfile("Tff.vcd");
-    $dumpvars("+all");
-end
+    initial begin
+        $dumpfile("Tff.vcd");
+        $dumpvars("+all");
+    end
 
-initial begin
-    @(negedge clk) t = 1'b1;
-    rst_n <= 1;
-    @(negedge ~clk) t = 1'b0;
-    @(negedge clk) $finish;
-end
+    initial begin
+        @(negedge clk) t = 1'b1;
+        rst_n <= 1;
+        @(negedge ~clk) t = 1'b0;
+        @(negedge clk) $finish;
+    end
 
 endmodule

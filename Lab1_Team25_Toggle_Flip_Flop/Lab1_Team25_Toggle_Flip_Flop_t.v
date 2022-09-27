bae97c6e30269f75a87f2f2957bd380622ab8a96
oohwee-yju <chris920318@gmail.com>
2022-09-28 04:54:42 +0800

    TFF testbench completed

--- Lab1_Team25_Toggle_Flip_Flop/Lab1_Team25_Toggle_Flip_Flop_t.v
+++ Lab1_Team25_Toggle_Flip_Flop/Lab1_Team25_Toggle_Flip_Flop_t.v
@@ -21,25 +21,91 @@ module Toggle_Flip_Flop_t;
     end
 
     initial begin
+	// test (t, rst_n, q) = (1, 1, 0);
         @(negedge clk) t = 1'b1;
         rst_n <= 1;
         @(negedge ~clk) t = 1'b0;
-	@(negedge clk) t = 1'b1;
-	rst_n <= 0;
+	// test (t, rst_n, q) = (1, 1, 1);
 	@(negedge clk) t = 1'b1;
         @(negedge ~clk) t = 1'b0;
+	// test (t, rst_n, q) = (1, 0, 0);
 	@(negedge clk) t = 1'b1;
+	rst_n <= 0;
+	@(negedge ~clk) t = 1'b0;
+	// test (t, rst_n, q) = (0, 0, 0);
+	@(negedge clk) t = 1'b0;
+        @(negedge ~clk) t = 1'b0;
+	// test (t, rst_n, q) = (0, 1, 0);
+	@(negedge clk) t = 1'b0;
 	rst_n <= 1;
-	@(negedge ~clk) t= 1'b0;
+        @(negedge ~clk) t = 1'b0;
+	// toggle q to be 1
 	@(negedge clk) t = 1'b1;
 	@(negedge ~clk) t = 1'b0;
+	// test (t, rst_n, q) = (0, 1, 1);
 	@(negedge clk) t = 1'b0;
+	@(negedge ~clk) t = 1'b0;
+	// test (t, rst_n, q) = (1, 0, 1);
+	@(negedge clk) t = 1'b1;
+	rst_n <= 0;
+	@(negedge ~clk) t = 1'b0;
+	// test (t, rst_n, q) = (0, 0, 1);
+	@(negedge clk) t = 1'b0;
+	rst_n <= 0;
+	@(negedge ~clk) t = 1'b0;
+
+	@(negedge clk) t = 1'b1;
+	
+	// test that clock is correct
+	// test (t, rst_n, q) = (1, 1, 0);
+        @(negedge ~clk) t = 1'b1;
+        rst_n <= 1;
+        @(negedge clk) t = 1'b0;
+	// test (t, rst_n, q) = (1, 1, 1);
+	@(negedge ~clk) t = 1'b1;
+        @(negedge clk) t = 1'b0;
+	// test (t, rst_n, q) = (1, 0, 0);
+	@(negedge ~clk) t = 1'b1;
+	rst_n <= 0;
+	@(negedge clk) t = 1'b0;
+	// test (t, rst_n, q) = (0, 0, 0);
+	@(negedge ~clk) t = 1'b0;
+        @(negedge clk) t = 1'b0;
+	// test (t, rst_n, q) = (0, 1, 0);
+	@(negedge ~clk) t = 1'b0;
+	rst_n <= 1;
+        @(negedge clk) t = 1'b0;
+	// toggle q to be 1
+	@(negedge ~clk) t = 1'b1;
+	@(negedge clk) t = 1'b0;
+	// test (t, rst_n, q) = (0, 1, 1);
+	@(negedge ~clk) t = 1'b0;
+	@(negedge clk) t = 1'b0;
+	// test (t, rst_n, q) = (1, 0, 1);
+	@(negedge ~clk) t = 1'b1;
+	rst_n <= 0;
+	@(negedge clk) t = 1'b0;
+	// test (t, rst_n, q) = (0, 0, 1);
+	@(negedge ~clk) t = 1'b0;
+	rst_n <= 0;
+	@(negedge clk) t = 1'b0;
+
+	@(negedge clk) t = 1'b1;
+	rst_n <= 1;
 	@(negedge clk) t = 1'b1;
 	@(negedge clk) t = 1'b1;
 	@(negedge clk) t = 1'b1;
 	@(negedge clk) t = 1'b1;
 	@(negedge clk) t = 1'b1;
 	@(negedge clk) t = 1'b1;
+	@(negedge clk) t = 1'b1;
+	@(negedge clk) t = 1'b1;
+	@(negedge clk) t = 1'b1;
+	@(negedge clk) t = 1'b1;
+	@(negedge clk) t = 1'b1;
+	@(negedge clk) t = 1'b1;
+	@(negedge clk) t = 1'b1;
+	
         @(negedge clk) $finish;
     end
 

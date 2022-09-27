48d26835c813987bd1e30370547410ef533f027b
ShuChenLin <ch25585.leader.tw@gmail.com>
2022-09-27 23:50:45 +0800

    push pdf

--- Lab1_Team25_Crossbar_2x2_4bit/Lab1_Team25_Crossbar_2x2_4bit.v
+++ Lab1_Team25_Crossbar_2x2_4bit/Lab1_Team25_Crossbar_2x2_4bit.v
@@ -52,6 +52,7 @@ module Dmux_1x2_4bit(in, sel, y1, y2);
 endmodule
 
 module Crossbar_2x2_4bit(in1, in2, control, out1, out2);
+
     input [4-1:0] in1, in2;
     input control;
     output [4-1:0] out1, out2;

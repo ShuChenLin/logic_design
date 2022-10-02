54a5526caf87de3c920361504546bb10e73460c5
oohwee-yju <chris920318@gmail.com>
2022-10-02 22:57:39 +0800

    module name added

--- lab2/Lab2_Team25_Decode_And_Execute/Lab2_Team25_Decode_And_Execute.v
+++ lab2/Lab2_Team25_Decode_And_Execute/Lab2_Team25_Decode_And_Execute.v
@@ -23,7 +23,7 @@ module myNot(out, a);
     input a;
     output out;
 
-    myGate (out, 1'b1, a);
+    myGate g0(out, 1'b1, a);
 
 endmodule
 
@@ -33,8 +33,8 @@ module myPass(out, a); // DO NOTHING
     output out;
 
     wire w;
-    myNot (w, a);
-    myNot (out, w);
+    myNot g0(w, a);
+    myNot g1(out, w);
 
 endmodule
 
@@ -45,8 +45,8 @@ module myAnd(out, a, b);
 
     wire rb;
 
-    myNot (rb, b);
-    myGate (out, a, rb);
+    myNot g0(rb, b);
+    myGate g1(out, a, rb);
 
 endmodule
 
@@ -57,9 +57,9 @@ module myOr (out, a, b);
     
     wire ra, w1;
     
-    myNot (ra, a);
-    myGate (w1, ra, b);
-    myNot (out, w1);
+    myNot g0(ra, a);
+    myGate g1(w1, ra, b);
+    myNot g2(out, w1);
     
 endmodule
 
@@ -70,11 +70,11 @@ module myXor (out, a, b);
     
     wire ra, rb, w1, w2;
     
-    myNot (ra, a);
-    myNot (rb, b);
-    myAnd (w1, ra, b);
-    myAnd (w2, a, rb);
-    myOr (out, w1, w2);
+    myNot g0(ra, a);
+    myNot g1(rb, b);
+    myAnd g2(w1, ra, b);
+    myAnd g3(w2, a, rb);
+    myOr g4(out, w1, w2);
     
 endmodule
 
@@ -85,8 +85,8 @@ module myXnor (out, a, b);
 
     wire w;
 
-    myXor (w, a, b);
-    myNot (out, w);
+    myXor g0(w, a, b);
+    myNot g1(out, w);
 
 endmodule
 
@@ -105,21 +105,21 @@ module MUX(out, sel, a, b); // MUX 2 to 1, 4 bit
     wire rsel;
     wire [4-1:0] w1, w2;
 
-    myNot (rsel, sel);
-    myAnd (w1[3], a[3], rsel);
-    myAnd (w1[2], a[2], rsel);
-    myAnd (w1[1], a[1], rsel);
-    myAnd (w1[0], a[0], rsel);
+    myNot g0(rsel, sel);
+    myAnd g1(w1[3], a[3], rsel);
+    myAnd g2(w1[2], a[2], rsel);
+    myAnd g3(w1[1], a[1], rsel);
+    myAnd g4(w1[0], a[0], rsel);
 
-    myAnd (w2[3], b[3], sel);
-    myAnd (w2[2], b[2], sel);
-    myAnd (w2[1], b[1], sel);
-    myAnd (w2[0], b[0], sel);
+    myAnd g5(w2[3], b[3], sel);
+    myAnd g6(w2[2], b[2], sel);
+    myAnd g7(w2[1], b[1], sel);
+    myAnd g8(w2[0], b[0], sel);
 
-    myOr (out[3], w1[3], w2[3]);
-    myOr (out[2], w1[2], w2[2]);
-    myOr (out[1], w1[1], w2[1]);
-    myOr (out[0], w1[0], w2[0]);
+    myOr g9(out[3], w1[3], w2[3]);
+    myOr g10(out[2], w1[2], w2[2]);
+    myOr g11(out[1], w1[1], w2[1]);
+    myOr g12(out[0], w1[0], w2[0]);
 
 endmodule
 
@@ -128,8 +128,8 @@ module HA (out, cout, a, b); // HALF ADDER
     input a, b;
     output out, cout;
 
-    myAnd (cout, a, b);
-    myXor (out, a, b);
+    myAnd g0(cout, a, b);
+    myXor g1(out, a, b);
 
 endmodule 
 
@@ -140,9 +140,9 @@ module FA(out, cout, cin, a, b); // Full Adder
 
     wire w, c1, c2;
 
-    HA (w, c1, a, b); 
-    HA (out, c2, w, cin);
-    myOr (cout, c1, c2);
+    HA g0(w, c1, a, b); 
+    HA g1(out, c2, w, cin);
+    myOr g2(cout, c1, c2);
     
 endmodule
 
@@ -151,9 +151,9 @@ module cmp(le, eq, ge, a, b); // comparator, 1 bit
     input a, b;
     output le, eq, ge;
 
-    myGate (ge, a, b);
-    myGate (le, b, a);
-    myXnor (eq, a, b);
+    myGate g0(ge, a, b);
+    myGate g1(le, b, a);
+    myXnor g2(eq, a, b);
 
 endmodule
 
@@ -166,38 +166,38 @@ module COMPARATOR(le, eq, ge, a, b); // LESS THAN, return 1 if a < b, 4 bit
     wire le0, le1, le2, le3;
     wire ge0, ge1, ge2, ge3;
 
-    cmp (le3, eq3, ge3, a[3], b[3]);
-    cmp (le2, eq2, ge2, a[2], b[2]);
-    cmp (le1, eq1, ge1, a[1], b[1]);
-    cmp (le0, eq0, ge0, a[0], b[0]);
+    cmp c0(le3, eq3, ge3, a[3], b[3]);
+    cmp c1(le2, eq2, ge2, a[2], b[2]);
+    cmp c2(le1, eq1, ge1, a[1], b[1]);
+    cmp c3(le0, eq0, ge0, a[0], b[0]);
     
-    myPass(pq3, eq3);
-    myAnd (pq2, eq2, pq3);
-    myAnd (pq1, eq1, pq2);
-    myAnd (pq0, eq0, pq1);
+    myPass g0(pq3, eq3);
+    myAnd g1(pq2, eq2, pq3);
+    myAnd g2(pq1, eq1, pq2);
+    myAnd g3(pq0, eq0, pq1);
 
     wire res0, res1, res2, res3;
 
-    myPass (res3, le3);
-    myAnd (res2, le2, pq3);
-    myAnd (res1, le1, pq2);
-    myAnd (res0, le0, pq1);
+    myPass g4(res3, le3);
+    myAnd g5(res2, le2, pq3);
+    myAnd g6(res1, le1, pq2);
+    myAnd g7(res0, le0, pq1);
 
     wire mg0, mg1, mg2, mg3;
 
-    myPass (mg3, res3);
-    myOr (mg2, mg3, res2);
-    myOr (mg1, mg2, res1);
-    myOr (mg0, mg1, res0);
+    myPass g8(mg3, res3);
+    myOr g9(mg2, mg3, res2);
+    myOr g10(mg1, mg2, res1);
+    myOr g11(mg0, mg1, res0);
 
-    myPass (le, mg0);
-    myPass (eq, pq0);
+    myPass g12(le, mg0);
+    myPass g13(eq, pq0);
 
     wire nle, neq;
 
-    myNot (nle, le);
-    myNot (neq, eq);
-    myAnd (ge, nle, neq);
+    myNot g14(nle, le);
+    myNot g15(neq, eq);
+    myAnd g16(ge, nle, neq);
 
 endmodule
 
@@ -210,10 +210,10 @@ module BITWISE_OR (out, a, b);
     input [4-1:0] a, b;
     output [4-1:0] out;
 
-    myOr (out[0], a[0], b[0]);
-    myOr (out[1], a[1], b[1]);
-    myOr (out[2], a[2], b[2]);
-    myOr (out[3], a[3], b[3]);
+    myOr g0(out[0], a[0], b[0]);
+    myOr g1(out[1], a[1], b[1]);
+    myOr g2(out[2], a[2], b[2]);
+    myOr g3(out[3], a[3], b[3]);
 
 endmodule
 
@@ -222,10 +222,10 @@ module BITWISE_AND (out, a, b);
     input [4-1:0] a, b;
     output [4-1:0] out;
 
-    myAnd (out[0], a[0], b[0]);
-    myAnd (out[1], a[1], b[1]);
-    myAnd (out[2], a[2], b[2]);
-    myAnd (out[3], a[3], b[3]);
+    myAnd g0(out[0], a[0], b[0]);
+    myAnd g1(out[1], a[1], b[1]);
+    myAnd g2(out[2], a[2], b[2]);
+    myAnd g3(out[3], a[3], b[3]);
 
 endmodule
 
@@ -234,10 +234,10 @@ module BITWISE_NOT (out, a);
     input [4-1:0] a;
     output [4-1:0] out;
 
-    myNot (out[3], a[3]);
-    myNot (out[2], a[2]);
-    myNot (out[1], a[1]);
-    myNot (out[0], a[0]);
+    myNot g0(out[3], a[3]);
+    myNot g1(out[2], a[2]);
+    myNot g2(out[1], a[1]);
+    myNot g3(out[0], a[0]);
 
 endmodule
 
@@ -248,10 +248,10 @@ module ADD(out, a, b); // a+b, 4 bit
 
     wire co0, co1, co2, co3;
 
-    HA (out[0], c0, a[0], b[0]);
-    FA (out[1], c1, c0, a[1], b[1]);
-    FA (out[2], c2, c1, a[2], b[2]);
-    FA (out[3], c3, c2, a[3], b[3]);
+    HA g0(out[0], c0, a[0], b[0]);
+    FA g1(out[1], c1, c0, a[1], b[1]);
+    FA g2(out[2], c2, c1, a[2], b[2]);
+    FA g3(out[3], c3, c2, a[3], b[3]);
 
 endmodule
 
@@ -262,8 +262,8 @@ module COMPLEMENT (out, a); // COMPLEMENT OF A INT, 4 bit
 
     wire [4-1:0] w;
 
-    BITWISE_NOT(w, a);
-    ADD(out, w, 4'b0001);
+    BITWISE_NOT g0(w, a);
+    ADD g1(out, w, 4'b0001);
 
 endmodule
 
@@ -273,8 +273,8 @@ module SUB (out, a, b); // a-b, 4 bit
     output [4-1:0] out;
 
     wire [4-1:0] nb;
-    COMPLEMENT(nb, b);
-    ADD (out, a, nb);
+    COMPLEMENT g0(nb, b);
+    ADD g1(out, a, nb);
 
 endmodule
 
@@ -284,10 +284,10 @@ module RIGHT_SHIFT(out, a);
     input [4-1:0] a;
     output [4-1:0] out;
 
-    myPass (out[3], a[3]); 
-    myPass (out[2], a[3]);
-    myPass (out[1], a[2]);
-    myPass (out[0], a[1]);
+    myPass g0(out[3], a[3]); 
+    myPass g1(out[2], a[3]);
+    myPass g2(out[1], a[2]);
+    myPass g3(out[0], a[1]);
 
 endmodule
 
@@ -297,10 +297,10 @@ module LEFT_SHIFT(out, a);
     input [4-1:0] a;
     output [4-1:0] out;
 
-    myPass (out[3], a[2]); 
-    myPass (out[2], a[1]);
-    myPass (out[1], a[0]);
-    myPass (out[0], a[3]);
+    myPass g0(out[3], a[2]); 
+    myPass g1(out[2], a[1]);
+    myPass g2(out[1], a[0]);
+    myPass g3(out[0], a[3]);
 
 endmodule
 
@@ -312,12 +312,12 @@ module COMPARE_LT(out, a, b);
 
     wire le, eq, ge;
 
-    COMPARATOR(le, eq, ge, a, b);
+    COMPARATOR g0(le, eq, ge, a, b);
 
-    myPass(out[3], 1'b1);
-    myPass(out[2], 1'b0);
-    myPass(out[1], 1'b1);
-    myPass(out[0], le);
+    myPass g1(out[3], 1'b1);
+    myPass g2(out[2], 1'b0);
+    myPass g3(out[1], 1'b1);
+    myPass g4(out[0], le);
 
 endmodule
 
@@ -331,10 +331,10 @@ module COMPARE_EQ(out, a, b);
 
     COMPARATOR(le, eq, ge, a, b);
 
-    myPass(out[3], 1'b1);
-    myPass(out[2], 1'b1);
-    myPass(out[1], 1'b1);
-    myPass(out[0], eq);
+    myPass g0(out[3], 1'b1);
+    myPass g1(out[2], 1'b1);
+    myPass g2(out[1], 1'b1);
+    myPass g3(out[0], eq);
 
 endmodule
 
@@ -354,24 +354,24 @@ module Decode_And_Execute(rs, rt, sel, rd);
 
     wire [4-1:0] r000, r001, r010, r011, r100, r101, r110, r111;
 
-    SUB (r000, rs, rt);
-    ADD (r001, rs, rt);
-    BITWISE_OR (r010, rs, rt);
-    BITWISE_AND (r011, rs, rt);
-    RIGHT_SHIFT (r100, rt);
-    LEFT_SHIFT (r101, rs);
-    COMPARE_LT (r110, rs, rt);
-    COMPARE_EQ (r111, rs, rt);
+    SUB g0(r000, rs, rt);
+    ADD g1(r001, rs, rt);
+    BITWISE_OR g2(r010, rs, rt);
+    BITWISE_AND g3(r011, rs, rt);
+    RIGHT_SHIFT g4(r100, rt);
+    LEFT_SHIFT g5(r101, rs);
+    COMPARE_LT g6(r110, rs, rt);
+    COMPARE_EQ g7(r111, rs, rt);
 
     wire L, R, LL, LR, RL, RR;
 
-    MUX (rd, sel[2], L, R);
-    MUX (L, sel[1], LL, LR);
-    MUX (R, sel[1], RL, RR);
-    MUX (LL, sel[0], r000, r001);
-    MUX (LR, sel[0], r010, r011);
-    MUX (RL, sel[0], r100, r101);
-    MUX (RR, sel[0], r110, r111);
+    MUX g8(rd, sel[2], L, R);
+    MUX g9(L, sel[1], LL, LR);
+    MUX g10(R, sel[1], RL, RR);
+    MUX g11(LL, sel[0], r000, r001);
+    MUX g12(LR, sel[0], r010, r011);
+    MUX g13(RL, sel[0], r100, r101);
+    MUX g14(RR, sel[0], r110, r111);
 
 endmodule
 

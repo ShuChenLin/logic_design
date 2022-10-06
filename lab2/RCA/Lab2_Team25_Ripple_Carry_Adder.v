7327a2355a5d98752fee5b93e342b08963e23ba2
ShuChenLin <ch25585.leader.tw@gmail.com>
2022-10-06 16:27:38 +0800

    renew

--- lab2/RCA/Lab2_Team25_Ripple_Carry_Adder.v
+++ lab2/RCA/Lab2_Team25_Ripple_Carry_Adder.v
@@ -1,70 +1,79 @@
 `timescale 1ns/1ps
 
-module myNot(o, a);
-    
-    input a;
+module my_And(o, x, y);
+    input x, y;
     output o;
 
-    nand ha(o, a, a);
+    wire tmp1;
+
+    nand a0(tmp1, x, y);
+    nand a1(o, tmp1, tmp1);
 
 endmodule
 
-module myAnd(o, a, b);
-    
-    input a, b;
-    output o;
-
-    nand ha(tmp, a, b);
-    myNot ka(o, tmp);
-
-endmodule
-
-module myOr(o, a, b);
-    
-    input a, b;
+module my_Or(o, x, y);
+    input x, y;
     output o;
 
     wire tmp1, tmp2;
 
-    myNot fi(tmp1, a);
-    myNot se(tmp2, b);
-    nand thir(o, tmp1, tmp2);
+    nand o0(tmp1, x, x);
+    nand o1(tmp2, y, y);
+    nand o2(o, tmp1, tmp2);
 
 endmodule
 
-module myXor(o, a, b);
-
-    input a, b;
+module my_Xor(o, x, y);
+    input x, y;
     output o;
 
-    wire na, nb;
+    wire tmp1_Xor, tmp2_Xor, tmp3_Xor;
 
-    myNot fi(na, a);
-    myNot se(nb, b);
-    myAnd bu(tmp1, a, nb);
-    myAnd bu1(tmp2, b, na);
-    myOr ka(o, tmp1, tmp2);
+    nand x0(tmp1_Xor, x, y);
+    nand x1(tmp2_Xor, x, tmp1_Xor);
+    nand x3(tmp3_Xor, y, tmp1_Xor);
+    nand x4(o, tmp2_Xor, tmp3_Xor);
 
 endmodule
 
-module Half_Adder(sum, cout, a, b);
-    
+module Majority(a, b, c, out);
+    input a, b, c;
+    output out;
+
+    wire tmp1, tmp2, tmp3, tmp4;
+
+    my_And k0(tmp1, a, b);
+    my_And k1(tmp2, a, c);
+    my_And k2(tmp3, b, c);
+    my_Or k3(tmp4, tmp1, tmp2);
+    my_Or k4(out, tmp3, tmp4);
+
+endmodule
+
+module Half_Adder (a, b, cout, sum);
     input a, b;
-    output sum, cout;
+    output cout, sum;
 
-    myAnd fi(cout, a, b);
-    myXor se(sum, a, b);
+    wire tmp1, tmp2, tmp3;
+
+    nand x0(tmp1, a, b);
+    nand x1(tmp2, a, tmp1);
+    nand x3(tmp3, b, tmp1);
+    nand x4(sum, tmp2, tmp3);
+    nand x6(cout, tmp1, tmp1);
+
 
 endmodule
 
-module Full_Adder(sum, cout, a, b, cin);
-
+module Full_Adder (a, b, cin, cout, sum);
     input a, b, cin;
-    output sum, cout;
-    
-    Half_Adder h0(tmp1, tmp2, a, b);
-    Half_Adder h1(sum, tmp3, tmp1, cin);
-    myOr h2(cout, tmp2, tmp3);
+    output cout, sum;
+
+    wire tmp1;
+
+    Majority M0(a, b, cin, cout);
+    my_Xor M1(tmp1, a, b);
+    my_Xor M2(sum, tmp1, cin);
 
 endmodule
 

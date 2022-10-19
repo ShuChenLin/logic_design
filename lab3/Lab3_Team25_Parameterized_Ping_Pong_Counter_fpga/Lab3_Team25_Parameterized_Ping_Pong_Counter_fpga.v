79ee23a45756c2d12aa22abdab9d6613e4c4de4d
ShuChenLin <ch25585.leader.tw@gmail.com>
2022-10-19 13:37:26 +0800

    debounced and one_pulse

--- lab3/Lab3_Team25_Parameterized_Ping_Pong_Counter_fpga/Lab3_Team25_Parameterized_Ping_Pong_Counter_fpga.v
+++ lab3/Lab3_Team25_Parameterized_Ping_Pong_Counter_fpga/Lab3_Team25_Parameterized_Ping_Pong_Counter_fpga.v
@@ -1,1 +1,32 @@
 `timescale 1ns/1ps
+
+module debounced(clk, pb, pb_debounced);
+
+    input clk, pb;
+    output pb_debounced;
+
+    reg [3:0] DFF;
+
+    always @(posedge clk) begin
+        DFF[3:1] <= DFF[2:0];
+        DFF[0] <= pb;
+    end
+
+    assign pb_debounced = ((DFF == 4'b1111) ? 1'b1 : 1'b0);
+
+endmodule
+
+module one_pause(clk, pb_debounced, pb_one_pulse):
+
+    input clk, pb_debounced;
+    output pb_one_pulse;
+
+    reg pb_one_pulse, pb_debounced_delay;
+
+    always @(posedge clk) begin
+        pb_one_pulse <= pb_debounced & (!pb_debounced_delay);
+
+        pb_debounced_delay <= pb_debounced;
+    end
+
+endmodule

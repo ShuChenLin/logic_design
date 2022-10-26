00c57eefa7ba35a1cb3685bd1ca1ae2780624d48
ShuChenLin <ch25585.leader.tw@gmail.com>
2022-10-26 19:29:11 +0800

    hello

--- lab3/Lab3_Team25_FIFO/nWaveLog/nWave.cmd
+++ lab3/Lab3_Team25_FIFO/nWaveLog/nWave.cmd
@@ -1,4 +1,4 @@
-wvResizeWindow -win $_nWave1 0 1 1440 853
+verdiSetActWin -win $_nWave1
 wvConvertFile -win $_nWave1 -o \
            "/users/course/2022F/LDL17700000/u110062208/logic_design/lab3/Lab3_Team25_FIFO/fifo.vcd.fsdb" \
            "/users/course/2022F/LDL17700000/u110062208/logic_design/lab3/Lab3_Team25_FIFO/fifo.vcd"
@@ -24,16 +24,6 @@ wvAddSignal -win $_nWave1 -group {"G2" \
 wvSelectSignal -win $_nWave1 {( "G1" 1 2 3 4 5 6 7 )} 
 wvSetPosition -win $_nWave1 {("G1" 7)}
 wvGetSignalClose -win $_nWave1
+wvResizeWindow -win $_nWave1 0 1 1920 1033
 wvZoomAll -win $_nWave1
-wvSetCursor -win $_nWave1 35370.216173 -snap {("G1" 1)}
-wvSetCursor -win $_nWave1 44758.606886 -snap {("G1" 1)}
-wvSetCursor -win $_nWave1 56330.344275 -snap {("G1" 1)}
-wvSetCursor -win $_nWave1 66592.073659 -snap {("G1" 1)}
-wvSetCursor -win $_nWave1 75543.795036 -snap {("G1" 1)}
-wvSetCursor -win $_nWave1 84932.185749 -snap {("G1" 1)}
-wvSetCursor -win $_nWave1 94538.911129 -snap {("G1" 1)}
-wvSetCursor -win $_nWave1 105018.975180 -snap {("G1" 1)}
-wvSetCursor -win $_nWave1 115062.369896 -snap {("G1" 1)}
-wvSetCursor -win $_nWave1 130127.461970 -snap {("G1" 1)}
-wvSelectSignal -win $_nWave1 {( "G1" 1 2 3 4 5 6 7 )} 
-wvSetRadix -win $_nWave1 -format UDec
+wvExit
--- lab3/Lab3_Team25_Multi_Bank_Memory/nWaveLog/nWave.cmd
+++ lab3/Lab3_Team25_Multi_Bank_Memory/nWaveLog/nWave.cmd
@@ -1,9 +1,9 @@
 wvConvertFile -win $_nWave1 -o \
-           "/users/course/2022F/LDL17700000/u110062208/logic_design/lab3/Lab3_Team25_Multi_Bank_Memory/test.vcd.fsdb" \
-           "/users/course/2022F/LDL17700000/u110062208/logic_design/lab3/Lab3_Team25_Multi_Bank_Memory/test.vcd"
+           "/users/course/2022F/LDL17700000/u110062208/logic_design/lab3/Lab3_Team25_Multi_Bank_Memory/multi_memory.vcd.fsdb" \
+           "/users/course/2022F/LDL17700000/u110062208/logic_design/lab3/Lab3_Team25_Multi_Bank_Memory/multi_memory.vcd"
 wvSetPosition -win $_nWave1 {("G1" 0)}
 wvOpenFile -win $_nWave1 \
-           {/users/course/2022F/LDL17700000/u110062208/logic_design/lab3/Lab3_Team25_Multi_Bank_Memory/test.vcd.fsdb}
+           {/users/course/2022F/LDL17700000/u110062208/logic_design/lab3/Lab3_Team25_Multi_Bank_Memory/multi_memory.vcd.fsdb}
 wvGetSignalOpen -win $_nWave1
 wvGetSignalSetScope -win $_nWave1 "/Multi_Bank_Memory_t"
 wvSetPosition -win $_nWave1 {("G1" 7)}
@@ -13,7 +13,7 @@ wvAddSignal -win $_nWave1 -group {"G1" \
 {/Multi_Bank_Memory_t/clk} \
 {/Multi_Bank_Memory_t/din\[7:0\]} \
 {/Multi_Bank_Memory_t/dout\[7:0\]} \
-{/Multi_Bank_Memory_t/radder\[10:0\]} \
+{/Multi_Bank_Memory_t/raddr\[10:0\]} \
 {/Multi_Bank_Memory_t/ren} \
 {/Multi_Bank_Memory_t/waddr\[10:0\]} \
 {/Multi_Bank_Memory_t/wen} \
@@ -24,8 +24,4 @@ wvSelectSignal -win $_nWave1 {( "G1" 1 2 3 4 5 6 7 )}
 wvSetPosition -win $_nWave1 {("G1" 7)}
 wvGetSignalClose -win $_nWave1
 wvZoomAll -win $_nWave1
-wvSelectSignal -win $_nWave1 {( "G1" 1 2 3 4 5 6 7 )} 
-wvSetRadix -win $_nWave1 -format UDec
-wvSetCursor -win $_nWave1 24938.670937 -snap {("G1" 3)}
-wvSetCursor -win $_nWave1 34885.028022 -snap {("G1" 3)}
 wvExit

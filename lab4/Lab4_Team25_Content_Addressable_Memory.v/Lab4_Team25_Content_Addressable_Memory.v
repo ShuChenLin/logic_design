cab9df6b94b36ea5e2fa6fe60d5046dd34c7e183
ShuChenLin <ch25585.leader.tw@gmail.com>
2022-11-02 16:45:35 +0800

    hello

--- lab4/Lab4_Team25_Content_Addressable_Memory.v/Lab4_Team25_Content_Addressable_Memory.v
+++ lab4/Lab4_Team25_Content_Addressable_Memory.v/Lab4_Team25_Content_Addressable_Memory.v
@@ -9,4 +9,18 @@ module Content_Addressable_Memory(clk, wen, ren, din, addr, dout, hit);
     output [3:0] dout;
     output hit;
 
+    reg [4-1:0] dout;
+    reg hit;
+    reg [8-1:0] memory [16-1:0];
+    wire [4-1:0] next_dout;
+    wire next_hit;
+
+    always @(posedge clk) begin
+        
+    end
+
+    always @(*) begin
+        
+    end
+
 endmodule

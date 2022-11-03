b747bcaf68ea77c1f75864ab4b46c1c6e309a657
oohwee-yju <chris920318@gmail.com>
2022-11-03 10:16:41 +0800

    Q3 done

---
+++ lab4/yju_basic/Q3/INCA_libs/irun.lnx86.15.20.nc/files.ts
@@ -0,0 +1,2 @@
+1667441654 /users/course/2022F/LDL17700000/u110062205/logic_design/lab4/yju_basic/Q3/q3_t.v
+1667441565 /users/course/2022F/LDL17700000/u110062205/logic_design/lab4/yju_basic/Q3/q3.v

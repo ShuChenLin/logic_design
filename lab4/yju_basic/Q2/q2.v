3141b1259a94cd9193792a39076b524c63e8ed49
oohwee-yju <chris920318@gmail.com>
2022-11-03 09:42:01 +0800

    yju_basic q2

--- lab4/yju_basic/Q2/q2.v
+++ lab4/yju_basic/Q2/q2.v
@@ -1,11 +1,11 @@
 `timescale 1ns/1ps
 
-`define S0 3'b000;
-`define S1 3'b001;
-`define S2 3'b010;
-`define S3 3'b011;
-`define S4 3'b100;
-`define S5 3'b101;
+//`define 3'b000 3'b000;
+//`define 3'b001 3'b001;
+//`define 3'b010 3'b010;
+//`define 3'b011 3'b011;
+//`define 3'b100 3'b100;
+//`define 3'b101 3'b101;
 
 module mealy_machine(clk, rst_n, in, state, out);
 
@@ -15,32 +15,32 @@ module mealy_machine(clk, rst_n, in, state, out);
 
     always @(*) begin
         case ({state, in}) begin
-            {S0, 1'b0}: out =  1'b0;
-            {S0, 1'b1}: out =  1'b1;
-            {S1, 1'b0}: out =  1'b1;
-            {S1, 1'b1}: out =  1'b1;
-            {S2, 1'b0}: out =  1'b1;
-            {S2, 1'b1}: out =  1'b0;
-            {S3, 1'b0}: out =  1'b1;
-            {S3, 1'b1}: out =  1'b0;
-            {S4, 1'b0}: out =  1'b1;
-            {S4, 1'b1}: out =  1'b1;
-            {S5, 1'b0}: out =  1'b0;
-            {S5, 1'b1}: out =  1'b0;
+            {3'b000, 1'b0}: out =  1'b0;
+            {3'b000, 1'b1}: out =  1'b1;
+            {3'b001, 1'b0}: out =  1'b1;
+            {3'b001, 1'b1}: out =  1'b1;
+            {3'b010, 1'b0}: out =  1'b1;
+            {3'b010, 1'b1}: out =  1'b0;
+            {3'b011, 1'b0}: out =  1'b1;
+            {3'b011, 1'b1}: out =  1'b0;
+            {3'b100, 1'b0}: out =  1'b1;
+            {3'b100, 1'b1}: out =  1'b1;
+            {3'b101, 1'b0}: out =  1'b0;
+            {3'b101, 1'b1}: out =  1'b0;
         end
     end
 
     always @(posedge clk) begin
         if (!rst_n) begin
-            state <= S0;
+            state <= 3'b000;
         end else begin
             case (state) begin
-                S0: state <= (~in ? S0 : S2);
-                S1: state <= (~in ? S0 : S4);
-                S2: state <= (~in ? S5 : S1);
-                S3: state <= (~in ? S3 : S2);
-                S4: state <= (~in ? S2 : S4);
-                S5: state <= (~in ? S3 : S4);
+                3'b000: state <= (~in ? 3'b000 : 3'b010);
+                3'b001: state <= (~in ? 3'b000 : 3'b100);
+                3'b010: state <= (~in ? 3'b101 : 3'b001);
+                3'b011: state <= (~in ? 3'b011 : 3'b010);
+                3'b100: state <= (~in ? 3'b010 : 3'b100);
+                3'b101: state <= (~in ? 3'b011 : 3'b100);
             end
         end
     end

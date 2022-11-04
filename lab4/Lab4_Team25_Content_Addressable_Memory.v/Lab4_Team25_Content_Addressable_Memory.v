a538973825886cc705a20689664319e49f23b293
ShuChenLin <ch25585.leader.tw@gmail.com>
2022-11-04 23:36:22 +0800

    4-1

--- lab4/Lab4_Team25_Content_Addressable_Memory.v/Lab4_Team25_Content_Addressable_Memory.v
+++ lab4/Lab4_Team25_Content_Addressable_Memory.v/Lab4_Team25_Content_Addressable_Memory.v
@@ -9,18 +9,44 @@ module Content_Addressable_Memory(clk, wen, ren, din, addr, dout, hit);
     output [3:0] dout;
     output hit;
 
-    reg [4-1:0] dout;
-    reg hit;
+    reg [4-1:0] dout, tmp_out;
+    reg hit, found;
     reg [8-1:0] memory [16-1:0];
     wire [4-1:0] next_dout;
-    wire next_hit;
 
     always @(posedge clk) begin
-        
+        if (ren) begin
+            if (hit) begin
+                dout <= tmp_out;  
+            end else begin
+                dout <= 4'b0;
+            end
+        end else if (wen) begin
+            memory[addr] <= din;
+            dout <= 4'b0;
+        end
     end
 
     always @(*) begin
-        
+        hit = 0;
+        found = 0;
+        if (ren) begin
+            for (i = 0; i < 16; i = i + 1) begin
+                if (memory[i] == din && !found) begin
+                    hit = 1'b1;
+                    found = 1'b1;
+                    tmp_out = i;
+                end else begin
+                    hit = hit;
+                    found = found;
+                    tmp_out = tmp_out;
+                end
+            end
+        end else begin
+            hit = 1'b0;
+            found = 1'b0;
+            tmp_out = 4'b0;
+        end
     end
 
 endmodule

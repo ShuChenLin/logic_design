21a425553f7c017aebed3c4455065845700256b3
ShuChenLin <ch25585.leader.tw@gmail.com>
2022-11-06 19:16:58 +0800

    q4 done

---
+++ lab4/Lab4_Team25_Mealy_Sequence_Detector/Lab4_Team25_Mealy_Sequence_Detector_t.v
@@ -0,0 +1,53 @@
+`timescale 1ns/1ps
+
+module mealy_t;
+    
+    reg clk = 0, rst_n = 0;
+    reg in = 0;
+    wire dec;
+
+    parameter cyc=10;
+
+    always#(cyc/2)clk = ~clk;
+
+    Mealy_Sequence_Detector m1(
+        .clk(clk),
+        .rst_n(rst_n),
+        .in(in),
+        .dec(dec)
+    );
+
+    initial begin
+        $dumpfile("msd.vcd");
+        $dumpvars("+all");
+    end
+
+    initial begin
+        @(negedge clk);
+            rst_n = 0;
+            in = 0;
+
+        @(negedge clk) 
+            rst_n = 1;
+            in = 0;
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 1;
+
+        @(negedge clk) in = 0;        
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 0;
+            
+        @(negedge clk) in = 1;        
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 0;
+        @(negedge clk) in = 1;
+
+        @(negedge clk) in = 0;        
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 0;
+        @(negedge clk) in = 0;
+        @(negedge clk) $finish;
+    end
+endmodule

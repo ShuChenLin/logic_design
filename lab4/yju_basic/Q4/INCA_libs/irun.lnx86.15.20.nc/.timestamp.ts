f566509a5e34c604885ad7b8b5200efedf9674f0
oohwee-yju <chris920318@gmail.com>
2022-11-06 19:30:36 +0800

    tested

---
+++ lab4/yju_basic/Q4/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
@@ -0,0 +1,2 @@
+1667442128 /users/course/2022F/LDL17700000/u110062205/logic_design/lab4/yju_basic/Q4/q4_t.v
+1667442128 /users/course/2022F/LDL17700000/u110062205/logic_design/lab4/yju_basic/Q4/q4.v

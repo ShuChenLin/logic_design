dbf8edb6faf0fb3d1419eecacfe01f58f4ac7291
ShuChenLin <ch25585.leader.tw@gmail.com>
2022-11-19 00:32:58 +0800

    5-2

--- lab5/Lab5_Team25_Traffic_Light_Controller/Lab5_Team25_Traffic_Light_Controller.v
+++ lab5/Lab5_Team25_Traffic_Light_Controller/Lab5_Team25_Traffic_Light_Controller.v
@@ -1,16 +1,17 @@
 `timescale 1ns/1ps
 
-module Traffic_Light_Controller (clk, rst_n, lr_has_car, hw_light, lr_light);
+module Traffic_Light_Controller (clk, rst_n, lr_has_car, hw_light, lr_light, state);
 
     input clk, rst_n;
     input lr_has_car;
     output [2:0] hw_light;
     output [2:0] lr_light;
+    output [2:0] state;
 
     reg [2:0] state, next_state;
     reg [7:0] hg, hg_next, lg, lg_next;
     reg [4:0] hy, hy_next, ly, ly_next;
-    reg hr, hr_next, lr, lr_next;
+    reg [2:0] hw_light, lr_light;
 
     parameter HGLR = 3'b000;
     parameter HYLR = 3'b001;
@@ -18,78 +19,95 @@ module Traffic_Light_Controller (clk, rst_n, lr_has_car, hw_light, lr_light);
     parameter HRLG = 3'b011;
     parameter HRLY = 3'b100;
     parameter HRLR2 = 3'b101;
+    parameter RED = 3'b001;
+    parameter YEL = 3'b010;
+    parameter GRE = 3'b100;
+    
+    always @(*) begin
+        case (state)
+            HGLR : begin
+                hw_light = GRE;
+                lr_light = RED;
+            end
+            HYLR : begin
+                hw_light = YEL;
+                lr_light = RED;
+            end
+            HRLR : begin
+                hw_light = RED;
+                lr_light = RED;
+            end
+            HRLG : begin
+                hw_light = RED;
+                lr_light = GRE;
+            end
+            HRLY : begin
+                hw_light = RED;
+                lr_light = YEL;
+            end
+            HRLR2 : begin
+                hw_light = RED;
+                lr_light = RED;
+            end
+            default : begin
+                hw_light = GRE;
+                lr_light = RED;
+            end
+        endcase
+    end
 
     always @(*) begin
         case (state)
             HGLR : begin
                 hg_next = hg + 1; lg_next = 0;
                 hy_next = 0; ly_next = 0;
-                hr_next = 0; lr_next = 0;    
             end
             HYLR : begin
                 hy_next = hy + 1; ly_next = 0;
                 hg_next = 0; lg_next = 0;
-                hr_next = 0; lr_next = 0; 
-            end
-            HRLR : begin
-                hr_next = hr + 1; lr_next = 0;
-                hg_next = 0; lg_next = 0;
-                hy_next = 0; ly_next = 0;
             end
             HRLG : begin
                 lg_next = lg + 1; hg_next = 0;
                 ly_next = 0; hy_next = 0;
-                lr_next = 0; hr_next = 0;
             end
             HRLY : begin
                 ly_next = ly + 1; hy_next = 0;
                 lg_next = 0; hg_next = 0;
-                lr_next = 0; hr_next = 0;
-            end
-            HRLR2 : begin
-                lr_next = lr + 1; hr_next = 0;
-                hg_next = 0; lg_next = 0;
-                hr_next = 0; lr_next = 0;
             end
             default : begin
                 hg_next = 0; lg_next = 0;
-                hr_next = 0; lr_next = 0;        
-                hy_next = 0; hr_next = 0;
+                hy_next = 0; hy_next = 0;
             end
         endcase
     end
 
-    always @(posedge clk) begin
+    always @(*) begin
         case (state)
             HGLR : begin
-                if (lr_has_car && hg >= 3) begin
+                if (lr_has_car && hg >= 8'b00000011) begin
                     next_state = HYLR;
                 end else next_state = HGLR;
             end
             HYLR : begin
-                if (hy == 2) begin
+                if (hy == 5'b010) begin
                     next_state = HRLR;
                 end else next_state = HYLR;
             end
             HRLR : begin
-                if (hr) begin
-                    next_state = HRLR;
-                end else next_stae = HRLG;
+                next_state = HRLG;
             end
             HRLG : begin
-                if (lg == 3) begin
-                    next_state = HRLG;
-                end else next_state = HRLY;
+                if (lg == 8'b00000011) begin
+                    next_state = HRLY;
+                end else next_state = HRLG;
             end
             HRLY : begin
-                if (ly == 2) begin
+                if (ly == 5'b010) begin
                     next_state = HRLR2;
                 end else next_state = HRLY;
             end
             HRLR2 : begin
-                if (lr) begin
-                    next_state = HGLR;
-                end else next_state = HRLR2;
+                next_state = HGLR;
             end
             default : begin
                 next_state = state;
@@ -100,8 +118,14 @@ module Traffic_Light_Controller (clk, rst_n, lr_has_car, hw_light, lr_light);
     always @(posedge clk) begin
         if (!rst_n) begin
             state <= HGLR;
+            hy <= 0; hg <= 0;
+            ly <= 0; lg <= 0;
+            hw_light <= GRE;
+            lr_light <= RED;
         end else begin
             state <= next_state;
+            hy <= hy_next; hg <= hg_next;
+            ly <= ly_next; lg <= lg_next;
         end
     end
 
---
+++ lab5/Lab5_Team25_Traffic_Light_Controller/Lab5_Team25_Traffic_Light_Controller_t.v
@@ -0,0 +1,74 @@
+`timescale 1ns/1ps
+
+module q2_t;
+    
+    reg clk = 0;
+    reg rst_n = 0;
+    reg lr_has_car = 0;
+    wire [2:0] hw_light, lr_light;
+    wire [2:0] state;
+
+    parameter cyc = 10;
+
+    always # (cyc/2) clk = ~clk;
+
+    Traffic_Light_Controller T1(
+        .clk(clk),
+        .rst_n(rst_n),
+        .lr_has_car(lr_has_car),
+        .hw_light(hw_light),
+        .state(state),
+        .lr_light(lr_light)
+    );
+
+    initial begin
+        $dumpfile("Traffic.vcd");
+        $dumpvars("+all");
+    end
+
+    initial begin
+        @(negedge clk)
+        rst_n = 0;
+        @(negedge clk)
+        rst_n = 1;
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        lr_has_car = 1;
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        lr_has_car = 0;
+        @(negedge clk);
+        @(negedge clk);
+        lr_has_car = 1;
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk); 
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        @(negedge clk);
+        $finish;
+    end
+
+endmodule

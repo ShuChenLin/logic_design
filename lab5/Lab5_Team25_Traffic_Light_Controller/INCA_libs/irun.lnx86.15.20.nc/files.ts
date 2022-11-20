bbd1e6c9f1c6fd91f127efe4df91c32b72374b94
ShuChenLin <ch25585.leader.tw@gmail.com>
2022-11-20 14:29:55 +0800

    5-2 tested

--- lab5/Lab5_Team25_Traffic_Light_Controller/INCA_libs/irun.lnx86.15.20.nc/files.ts
+++ lab5/Lab5_Team25_Traffic_Light_Controller/INCA_libs/irun.lnx86.15.20.nc/files.ts
@@ -1,2 +1,2 @@
-1668788905 /users/course/2022F/LDL17700000/u110062208/logic_design/lab5/Lab5_Team25_Traffic_Light_Controller/Lab5_Team25_Traffic_Light_Controller_t.v
-1668828546 /users/course/2022F/LDL17700000/u110062208/logic_design/lab5/Lab5_Team25_Traffic_Light_Controller/Lab5_Team25_Traffic_Light_Controller.v
+1668925581 /users/course/2022F/LDL17700000/u110062208/logic_design/lab5/Lab5_Team25_Traffic_Light_Controller/Lab5_Team25_Traffic_Light_Controller_t.v
+1668925558 /users/course/2022F/LDL17700000/u110062208/logic_design/lab5/Lab5_Team25_Traffic_Light_Controller/Lab5_Team25_Traffic_Light_Controller.v
--- lab5/Lab5_Team25_Traffic_Light_Controller/nWaveLog/nWave.cmd
+++ lab5/Lab5_Team25_Traffic_Light_Controller/nWaveLog/nWave.cmd
@@ -1,15 +1,15 @@
 verdiSetActWin -win $_nWave1
+wvResizeWindow -win $_nWave1 0 1 1440 853
 wvConvertFile -win $_nWave1 -o \
            "/users/course/2022F/LDL17700000/u110062208/logic_design/lab5/Lab5_Team25_Traffic_Light_Controller/Traffic.vcd.fsdb" \
            "/users/course/2022F/LDL17700000/u110062208/logic_design/lab5/Lab5_Team25_Traffic_Light_Controller/Traffic.vcd"
 wvSetPosition -win $_nWave1 {("G1" 0)}
 wvOpenFile -win $_nWave1 \
            {/users/course/2022F/LDL17700000/u110062208/logic_design/lab5/Lab5_Team25_Traffic_Light_Controller/Traffic.vcd.fsdb}
-wvResizeWindow -win $_nWave1 0 1 1440 853
 wvGetSignalOpen -win $_nWave1
 wvGetSignalSetScope -win $_nWave1 "/q2_t"
-wvSetPosition -win $_nWave1 {("G1" 6)}
-wvSetPosition -win $_nWave1 {("G1" 6)}
+wvSetPosition -win $_nWave1 {("G1" 5)}
+wvSetPosition -win $_nWave1 {("G1" 5)}
 wvAddSignal -win $_nWave1 -clear
 wvAddSignal -win $_nWave1 -group {"G1" \
 {/q2_t/clk} \
@@ -17,14 +17,11 @@ wvAddSignal -win $_nWave1 -group {"G1" \
 {/q2_t/lr_has_car} \
 {/q2_t/lr_light\[2:0\]} \
 {/q2_t/rst_n} \
-{/q2_t/state\[2:0\]} \
 }
 wvAddSignal -win $_nWave1 -group {"G2" \
 }
-wvSelectSignal -win $_nWave1 {( "G1" 1 2 3 4 5 6 )} 
-wvSetPosition -win $_nWave1 {("G1" 6)}
+wvSelectSignal -win $_nWave1 {( "G1" 1 2 3 4 5 )} 
+wvSetPosition -win $_nWave1 {("G1" 5)}
 wvGetSignalClose -win $_nWave1
 wvZoomAll -win $_nWave1
-wvSelectSignal -win $_nWave1 {( "G1" 1 2 3 4 5 6 )} 
-wvSetRadix -win $_nWave1 -format Bin
 wvExit

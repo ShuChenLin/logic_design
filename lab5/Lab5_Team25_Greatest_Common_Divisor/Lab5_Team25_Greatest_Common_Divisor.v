e5f7e1fcfb9bb4d5575c98a0c03d4c430540e65c
oohwee-yju <chris920318@gmail.com>
2022-11-23 16:21:56 +0800

    gcd

---
+++ lab5/Lab5_Team25_Greatest_Common_Divisor/Lab5_Team25_Greatest_Common_Divisor.v
@@ -0,0 +1,90 @@
+`timescale 1ns/1ps
+
+module Greatest_Common_Divisor (clk, rst_n, start, a, b, done, gcd);
+
+    input clk, rst_n;
+    input start;
+    input [15:0] a;
+    input [15:0] b;
+    output reg done;
+    output reg [15:0] gcd;
+    
+    parameter WAIT = 2'b00;
+    parameter CAL = 2'b01;
+    parameter FINISH = 2'b10;
+    
+    reg [15:0] A, B, next_A, next_B, next_gcd;
+    reg next_done;
+
+    reg [1:0] count, next_count;
+
+    always @(*) begin
+        if (state == WAIT) begin
+            next_count = 2'b0;
+            next_gcd = 16'b0;
+            next_done = 1'b0;
+            if (start == 1'b1) begin
+                next_A = a;
+                next_B = b;
+                next_state = CAL;
+            end else begin
+                next_state = WAIT; 
+            end
+        end else if (state == CAL) begin
+            next_count = 2'b0;
+            if (A == 0) begin
+                next_gcd = B;
+                next_done = 1'b1;
+                next_A = A;
+                next_B = B;
+            end else begin
+                if (B == 0) begin
+                    next_gcd = A;
+                    next_done = 1'b1;
+                    next_A = A;
+                    next_B = B;
+                end else if (A > B) begin
+                    next_gcd = 16'b0;
+                    next_done = 1'b0;
+                    next_A = A-B;
+                    next_B = B;
+                end else begin
+                    next_gcd = 16'b0;
+                    next_done = 1'b0;
+                    next_A = A;
+                    next_B = B-A;
+                end
+            end
+        end else if (state == FINISH) begin
+            if (count == 2'b0) begin
+                next_gcd = gcd;
+                next_done = done;
+                next_count = count + 1'b1;
+                next_state = FINISH;
+            end else begin
+                next_count = 2'b0;
+                next_gcd = 16'b0;
+                next_done = 1'b0;
+                next_state = WAIT;
+            end
+        end
+    end
+
+    always @(posedge clk) begin
+        if (rst_n == 1'b0) begin
+            A <= 16'b0;
+            B <= 16'b0;
+            gcd <= 16'b0;
+            done <= 1'b0;
+            count <= 2'b0;
+        end else begin
+            state <= next_state;
+            A <= next_A;
+            B <= next_B;
+            gcd <= next_gcd;
+            done <= next_done;
+            count <= next_count;
+        end
+    end
+    
+endmodule
--- lab5/myhdl_test/test.py
+++ lab5/myhdl_test/test.py
@@ -6,42 +6,56 @@ def Greatest_common_divisor(start, a, b, clk, rst_n, gcd, done):
     state = Signal(modbv(0, min=0, max=4))
     A = Signal(modbv(0)[16:])
     B = Signal(modbv(0)[16:])
+    next_A = Signal(modbv(0)[16:])
+    next_B = Signal(modbv(0)[16:])
+    next_gcd = Signal(modbv(0)[16:])
+    next_done = Signal(modbv(0))
 
-    @always_seq(clk.posedge, reset=rst_n)
-    def logic():
+    @always_comb
+    def logic0():
         if (state == 0):
-            gcd.next = 0
-            done.next = 0
+            next_gcd.next = 0
+            next_done.next = 0
             if (start == 1):
-                state.next = 1
-                A.next = a
-                B.next = b
+                next_state.next = 1
+                next_A.next = a
+                next_B.next = b
             else:
-                state.next = 0
+                next_state.next = 0
         elif (state == 1):
-            gcd.next = 0
-            done.next = 0
             if (A == 0):
-                state.next = 2
-                done.next = 1
-                gcd.next = B
+                next_state.next = 2
+                next_done.next = 1
+                next_gcd.next = B
             else:
                 if (B == 0):
-                    state.next = 2
-                    done.next = 1
-                    gcd.next = A
+                    next_state = 2
+                    next_done = 1
+                    next_gcd = A
                 else:
-                    state.next = 2
+                    next_state = 1
                     if (A > B):
-                        A.next = A-B
+                        next_A = A-B
+                        next_B = B
                     else:
-                        B.next = B-A
+                        next_B = B-A
+                        next_A = A
         elif (state == 2):
-            state.next = 3
+            next_state = 3
         elif (state == 3):
-            state.next = 0
-            
-    return logic
+            next_state = 0
+            next_gcd = 0
+            next_done = 0
+
+    @always_seq(clk.posedge, reset=rst_n)
+    def logic1():
+        state.next = next_state
+        gcd.next = next_gcd
+        done.next = next_done
+        A.next = next_A
+        B.next = next_B
+
+    return logic0, logic1
 
 def convertG(hdl, width=16):
     start = Signal(bool())

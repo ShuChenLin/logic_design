3c1b71429a4aa5ee09a7efd27dfc1650475a50b3
oohwee-yju <chris920318@gmail.com>
2022-11-24 12:40:37 +0800

    tb

--- lab5/Lab5_Team25_Booth_Multiplier_4bit/Lab5_Team25_Booth_Multiplier_4bit_t.v
+++ lab5/Lab5_Team25_Booth_Multiplier_4bit/Lab5_Team25_Booth_Multiplier_4bit_t.v
@@ -5,26 +5,25 @@ module q1_t;
     reg clk = 0;
     reg rst_n = 0;
     reg start = 0;
-    reg [15:0] a, b;
+    reg [3:9] a, b;
 
-    wire [15:0] gcd;
+    wire [7:0] p;
     wire done;
 
     parameter cyc = 10;
     always # (cyc / 2) clk = ~clk;
 
-    Greatest_Common_Divisor S1(
+    Booth_Multiplier_4bit S1(
         .clk(clk),
         .rst_n(rst_n),
         .start(start),
         .a(a),
         .b(b),
-        .done(done),
-        .gcd(gcd)
+        .p(p)
     );
 
     initial begin
-        $dumpfile("sliding_window.vcd");
+        $dumpfile("Booth_Multiplier.vcd");
         $dumpvars("+all");
     end
 

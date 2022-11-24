7c24e96fb642545def5a17e939003d8cb3bfff83
ShuChenLin <ch25585.leader.tw@gmail.com>
2022-11-24 23:16:54 +0800

    hello

--- lab5/Lab5_Team25_Sliding_Window_Sequence_Detector/Lab5_Team25_Sliding_Window_Sequence_Detector_t.v
+++ lab5/Lab5_Team25_Sliding_Window_Sequence_Detector/Lab5_Team25_Sliding_Window_Sequence_Detector_t.v
@@ -1,16 +1,16 @@
 `timescale 1ns/1ps
 
-module q1_t;
+module mealy_t;
     
-    reg clk = 0;
-    reg rst_n = 0;
+    reg clk = 0, rst_n = 0;
     reg in = 0;
     wire dec;
 
-    parameter cyc = 10;
-    always # (cyc / 2) clk = ~clk;
+    parameter cyc=10;
 
-    Sliding_Window_Sequence_Detector S1(
+    always#(cyc/2)clk = ~clk;
+
+    Mealy_Sequence_Detector m1(
         .clk(clk),
         .rst_n(rst_n),
         .in(in),
@@ -18,88 +18,96 @@ module q1_t;
     );
 
     initial begin
-        $dumpfile("sliding_window.vcd");
+        $dumpfile("msd.vcd");
         $dumpvars("+all");
     end
 
     initial begin
-        @(negedge clk) 
-        rst_n = 0;
-        in = 0;
-        @(negedge clk) 
-        rst_n = 1;
-        in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) 
-        rst_n = 0;
-        in = 0;
-        @(negedge clk)
-        rst_n = 1;
-        in = 0;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1; //dec = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1; //dec = 1;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 0;
-        @(negedge clk) in = 1;
-        @(negedge clk) in = 1;
         @(negedge clk);
-        $finish;
-    end
+            rst_n = 0;
+            in = 0;
 
+        @(negedge clk) 
+            rst_n = 1;
+            in = 0;
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 1;
+
+        @(negedge clk) in = 0;        
+        @(negedge clk) in = 0;
+        @(negedge clk) in = 0;
+        @(negedge clk) in = 1;
+
+        @(negedge clk) in = 0;        
+        @(negedge clk) in = 0;
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 0;
+
+        @(negedge clk) in = 0;        
+        @(negedge clk) in = 0;
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 1;
+
+        @(negedge clk) in = 0;        
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 0;
+        @(negedge clk) in = 0;
+
+        @(negedge clk) in = 0;        
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 0;
+        @(negedge clk) in = 1;
+
+        @(negedge clk) in = 0;        
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 0;
+
+        @(negedge clk) in = 0;        
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 1;
+
+        @(negedge clk) in = 1;        
+        @(negedge clk) in = 0;
+        @(negedge clk) in = 0;
+        @(negedge clk) in = 0;
+
+        @(negedge clk) in = 1;        
+        @(negedge clk) in = 0;
+        @(negedge clk) in = 0;
+        @(negedge clk) in = 1;
+
+        @(negedge clk) in = 1;        
+        @(negedge clk) in = 0;
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 0;
+
+        @(negedge clk) in = 1;        
+        @(negedge clk) in = 0;
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 1;
+
+        @(negedge clk) in = 1;        
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 0;
+        @(negedge clk) in = 0;
+
+        @(negedge clk) in = 1;        
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 0;
+        @(negedge clk) in = 1;
+            
+        @(negedge clk) in = 1;        
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 0;
+
+        @(negedge clk) in = 1;        
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 1;
+        @(negedge clk) in = 1;
+        @(negedge clk) $finish;
+    end
 endmodule

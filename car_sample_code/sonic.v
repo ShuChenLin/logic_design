ab7f566211aba77bc81926d882ce18f94be16a73
ShuChenLin <ch25585.leader.tw@gmail.com>
2022-12-02 16:42:09 +0800

    hello

--- car_sample_code/sonic.v
+++ car_sample_code/sonic.v
@@ -13,6 +13,8 @@ module sonic_top(clk, rst, Echo, Trig, stop);
 
     // [TO-DO] calculate the right distance to trig stop(triggered when the distance is lower than 40 cm)
     // Hint: using "dis"
+
+    assign stop = (dis <= 40);
  
 endmodule
 

e4b2ec2b4dff2e12b1f70e77636b1be587b25a65
ShuChenLin <ch25585.leader.tw@gmail.com>
2022-12-09 18:01:30 +0800

    nothing

--- lab6/chip2chip/chip2chip_slave/slave_control.v
+++ lab6/chip2chip/chip2chip_slave/slave_control.v
@@ -68,33 +68,27 @@ module slave_control(clk, rst_n, request, ack, data_in, notice, valid, data);
         case(state)
             state_wait_rqst: begin
                 next_state = (request == 1)? state_wait_to_send_ack: state_wait_rqst;
-                // next_notice = ???
-                next_ack = ack;
+                next_notice = 1;
+                next_ack = 0;
                 next_data = 0;
-                // next_start = (request == 1)? ???: ???;
+                next_start = (request == 1)? 1'b1: 1'b0;
             end
             state_wait_to_send_ack: begin
-                next_state = (done == 1)? state_send_ack : state_wait_to_send_ack;
-                // next_notice = ???
-                next_ack = ack;
-                next_data = data;
-                // next_start = ???
-            end
-            state_send_ack: begin
-                next_state = state_wait_data;
-                //next_notice = ???
-                next_ack = 1;
-                next_data = data;
-                // next_start = ???
-            enk
-            state_wait_data: begin
-                // next_state = (valid == 1)? ??? : ???;
-                // next_notice = ???
+                next_state = (done == 1)? state_wait_data : state_wait_to_send_ack;
+                next_notice = 1;
                 next_ack = 0;
+                next_data = data;
+                next_start = 0;
+            end
+            state_wait_data: begin
+                next_state = (valid == 1)? state_wait_rqst : state_wait_data;
+                next_notice = 0;
+                next_ack = 1;
                 next_data = data_in;
-                // next_start = ???
+                next_start = 0;
             end
             default: begin
+                next_state = state_wait_rqst;
             end
         endcase
     end

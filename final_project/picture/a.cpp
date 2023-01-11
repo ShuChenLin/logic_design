8f7d9c5df982f5b3d593742eb14e113518cd2c22
ShuChenLin <ch25585.leader.tw@gmail.com>
2023-01-11 21:00:21 +0800

    cpm

--- final_project/picture/a.cpp
+++ final_project/picture/a.cpp
@@ -4,12 +4,12 @@ using namespace std;
 signed main () {
     ifstream In;
     ofstream Out;
-    In.open("others.bin");
-    Out.open("others.txt");
+    In.open("CPM.bin");
+    Out.open("CPM.txt");
     string a;
     int cnt = 0;
     while (In >> a) {
-        Out << "assign otherletter[" << cnt << "] = 12'b" << a << ";\n";
+        Out << "assign CPM[" << cnt << "] = 12'b" << a << ";\n";
         cnt++;
     }
     return 0;
